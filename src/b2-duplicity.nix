1ee7b76661664988ec0fb6a2dc0e2feae4db5561
Ahmad Jarara <git@ajarara.io>
2020-09-13 10:11:36 -0400

    hide targets

--- src/b2-duplicity.nix
+++ src/b2-duplicity.nix
@@ -14,7 +14,7 @@ let
   b2-bucket = "b2://${keyId}:${applicationKey}@${bucketName}";
   b2-archive-cmds = map (archive: pkgs.writeShellScriptBin "b2-archive-${archive.name}" ''
     ${pkgs.duplicity}/bin/duplicity ${archive.local} \
-      ${lib.concatMapStrings (id: " --encrypt-key ${id}") keyIds} \
+      ${lib.concatMapStrings (id: " --hidden-encrypt-key ${id}") keyIds} \
       --use-agent \
       ${b2-bucket}/${archive.remote}
   '') archives;
@@ -22,7 +22,7 @@ let
   b2-restore-file-cmds = map (archive: pkgs.writeShellScriptBin "b2-restore-file-${archive.name}" ''
   ${pkgs.duplicity}/bin/duplicity --file-to-restore $1 \
     --use-agent \
-    ${b2-bucket}/${archive.remote}/$2
+    ${b2-bucket}/${archive.remote} "$2"
 '') archives;
     
 in {
