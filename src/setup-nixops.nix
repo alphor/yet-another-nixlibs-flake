7d91921390423633e5d4eccaaae21e1ff66b4d4d
Ahmad Jarara <git@ajarara.io>
2020-09-26 17:47:49 -0400

    Get nixops 1.7
    Get it off unstable. If in the future I need something else off this
    revision I'll pull this out into a /lib folder

--- src/setup-nixops.nix
+++ src/setup-nixops.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }:
 
-{
+let
+  unstable = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    # a head revision that passed CI as of 09/26/2020
+    rev = "58121fe1b085e201593f38e3b5eda0407dbd8e98";
+    sha256 = "1j0q5lzycl4p5fah5vgaqahh55xvcg1c1lyc2r1s6gh9vg8q3q47";
+  }) { };
+in {
   environment.systemPackages = [
-    pkgs.nixops
+    unstable.nixops
   ];
 }
