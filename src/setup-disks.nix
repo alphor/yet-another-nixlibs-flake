e4642ff324309105bbd9975a8720e43b65770164
Ahmad Jarara <git@ajarara.io>
2020-10-10 20:45:47 -0400

    Pull out filesystems
    The goal is to make this as simple as possible, like the emacs config

---
+++ src/setup-disks.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+    filesystems = {
+    "/" = {
+      device = "/dev/disk/by-label/root";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
+}
